81f29dec9d5e05630f6a073a93a910e6fcdbf8de
Soniya <bksoniya003@gmail.com>
2026-05-04 17:01:52 +0530

    fixed mail + admin

--- app.py
+++ app.py
@@ -34,8 +34,9 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # MAIL CONFIG
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
 
 app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
